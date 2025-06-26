8d76f156fce9d51ec11191a84967b0af4211f930
aifa <aigenerator.web1v2@gmail.com>
2025-06-26 15:10:33 +0100

    update SEO and icons2

--- config/appConfig.ts
+++ config/appConfig.ts
@@ -1,16 +1,14 @@
 import { AppConfig } from "@/types/app-config";
 
-// @/config/appConfig.ts
-const site_url = process.env.NEXT_PUBLIC_APP_URL;
-if (!site_url || !/^https?:\/\//.test(site_url)) {
-  throw new Error("NEXT_PUBLIC_APP_URL is not defined or invalid");
-}
+const site_url = process.env.NEXT_PUBLIC_APP_URL || "https://aifa.dev";
 
 export const appConfig: AppConfig = {
   name: "AIFA",
-  description: "...",
+  description:
+    "AI Agents in an Evolving and Self-Replicating Architecture for developing full-featured web applications, open source",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   manifest: "/manifest.json",
+
   mailSupport: "support@aifa.dev",
 };
